d842189c2e350c5d29aa5098d8c459a4667a5155
Manish Bag <manishbag007@gmail.com>
2022-03-01 20:01:59 +0530

    Refactored / Cleanup

--- timer.cpp
+++ timer.cpp
@@ -5,19 +5,18 @@ using namespace std;
 void beep_boop();
 int main(int argc, char **argv){
     if(argc < 2){
-        cerr<<"Please Specify Time In Minutes!"<<"\n";
+        cerr<<"Please Specify Time In Seconds!"<<"\n";
     }else if(argc > 2){
         cerr<<"Invalid Number Of Arguments Recieved!"<<"\n";
     }
     int counts = atoi(argv[1]);
     bool timer_ended = false;
-    // cout<<"COUNTS: "<<counts<<"\n";
     while(counts != 0){
         int hours = counts / 3600;
         int minsec = counts % 3600;
         int mins = minsec / 60;
         int secs = minsec % 60;
-        cout<<"HOURS: "<<hours<<" MINS: "<<mins<<" SECS: "<<secs<<"\n";
+        cout<<"H: "<<hours<<" M: "<<mins<<" S: "<<secs<<"\n";
         this_thread::sleep_for(chrono::milliseconds(1000));
         cout<<"\033[H\033[2J";
         counts--;
@@ -26,10 +25,6 @@ int main(int argc, char **argv){
     if(timer_ended){
         beep_boop();
     }
-    //When the timer finishes, this beep will played.
-    // while(true){
-    //     Beep(300,500);
-    // }
 }
 void beep_boop(){
     cout<<"\033[H\033[2J";
