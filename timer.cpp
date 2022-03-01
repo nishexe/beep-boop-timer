79cc684ad62574a8cbd3d68892f95095bf58ab47
Manish Bag <manishbag007@gmail.com>
2022-03-01 20:36:33 +0530

    Fixed if else.

--- timer.cpp
+++ timer.cpp
@@ -9,22 +9,23 @@ int main(int argc, char **argv){
         cerr<<"Please Specify Time In Seconds!"<<"\n";
     }else if(argc > 2){
         cerr<<"Invalid Number Of Arguments Recieved!"<<"\n";
-    }
-    int counts = atoi(argv[1]);
-    bool timer_ended = false;
-    while(counts != 0){
-        int hours = counts / 3600;
-        int minsec = counts % 3600;
-        int mins = minsec / 60;
-        int secs = minsec % 60;
-        cout<<"H:"<<hours<<" M:"<<mins<<" S:"<<secs<<"\n";
-        this_thread::sleep_for(chrono::milliseconds(1000));
-        system("cls");
-        counts--;
-    }
-    if (counts == 0) timer_ended = true;
-    if(timer_ended){
-        beep_boop();
+    }else{
+        int counts = atoi(argv[1]);
+        bool timer_ended = false;
+        while(counts != 0){
+            int hours = counts / 3600;
+            int minsec = counts % 3600;
+            int mins = minsec / 60;
+            int secs = minsec % 60;
+            cout<<"H:"<<hours<<" M:"<<mins<<" S:"<<secs<<"\n";
+            this_thread::sleep_for(chrono::milliseconds(1000));
+            system("cls");
+            counts--;
+        }
+        if (counts == 0) timer_ended = true;
+        if(timer_ended){
+            beep_boop();
+        }
     }
 }
 void beep_boop(){
